a9464e7b26f1d38891d0e0c746028ddf116cac25
j0z07b8 <jiaen.zhang@samsclub.com>
2021-10-28 18:30:27 -0700

    example to add a textfield

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// this is just a example, we should really consider using https://formik.org/docs/overview
+
 function App() {
   const [location, setLocation] = useState(0);
+  const [addr, setAddr] = useState("")
+
+  const handleChange = event => {
+    setAddr(event.target.value)
+    console.log(event.target.value)  // right click -> inspect -> console
+  }
 
   useEffect(() => {
     fetch('/location').then(res => res.json()).then(data => {
@@ -17,6 +25,13 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         ... THIS IS JUST A DEMO OF REACT ...
+        <p></p>
+
+        <form>
+          <label>
+            Address: <input type="text" value={addr} onChange={handleChange} size="60"/>
+          </label>
+        </form>
 
         <p>UC Irvine is at {location}.</p>
       </header>
