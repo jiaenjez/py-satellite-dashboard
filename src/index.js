f7124967473839fc19ebc0aedf104cf1150a1a0f
Chilly712 <44418247+Chilly712@users.noreply.github.com>
2022-01-19 17:42:11 -0600

    Bug fixes and reduce satellite selected for path calculation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Location from './components/Location'
+import Map from './components/Map'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Location />
+    <Map />
   </React.StrictMode>,
   document.getElementById('root')
 );
