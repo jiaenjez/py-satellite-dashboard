519d38f0ed576ae281932688dddda956399f796b
CubeSAT_GS <example@uci.edu>
2022-01-31 03:41:30 -0600

    Add Map component to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Time from './components/Time';
 import Logo from './components/Logo';
 import Location from './components/Location';
+import Map from './components/Map';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Location />
+        <Map />
         <Time />
         <Logo />
       </header>
