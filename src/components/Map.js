f26c18365e9de339c99a8e6ba7dec258d50707e9
CubeSAT_GS <example@uci.edu>
2022-01-31 13:04:55 -0600

    Setup .env file to keep secret KEY

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,14 +5,15 @@ import {
   withScriptjs, withGoogleMap, GoogleMap, Marker,
 } from 'react-google-maps';
 
+const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
 const Map = () => {
   const [marker, setMarker] = useState('');
   const defaultLocation = {lat: 33.6405, lng: -117.8443};
 
   const EmbeddedMap = compose(
       withProps({
-        googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyC3eh-avwD6U3CGDh6DjKuoCcGN9J1b8EA&v=3.exp&libraries=geometry,drawing,places',
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
         loadingElement: <div style={{height: `100%`}} />,
         containerElement: <div style={{height: `400px`}} />,
         mapElement: <div style={{height: `100%`}} />,
@@ -28,7 +29,7 @@ const Map = () => {
         props.currentLocation.lng = mouseEvent.latLng.lng();
         setMarker(`${mouseEvent.latLng.lat()}, ${mouseEvent.latLng.lng()}`);
       }}
-      defaultZoom={6}
+      defaultZoom={7}
       defaultCenter={defaultLocation}
     >
       {props.isMarkerShown && (
