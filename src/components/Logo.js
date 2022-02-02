365af8b16b29406b621fdd4ec2fc98b848dca1d7
CubeSAT_GS <44418247+Chilly712@users.noreply.github.com>
2022-02-02 17:57:30 -0600

    change Logo size

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,6 @@ import React from 'react';
 import logoImgFile from './cubesat.png';
 
 const Logo = () =>
-  <img src={logoImgFile} alt="Logo" height={'100px'} width={'100px'}/>;
+  <img src={logoImgFile} alt="Logo" height={'200px'} width={'200px'}/>;
 
 export default Logo;
