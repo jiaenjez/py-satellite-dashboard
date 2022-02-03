9c450637cff3c8d37a1bd267a865f15cc35ef4fb
j0z07b8 <jiaen.zhang@samsclub.com>
2022-02-03 03:21:06 -0600

    Show predicted upcoming pass

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,13 +2,17 @@
 import React, {useState} from 'react';
 import {compose, withProps} from 'recompose';
 import {
-  withScriptjs, withGoogleMap, GoogleMap, Marker,
+  withScriptjs, withGoogleMap, GoogleMap, Marker
 } from 'react-google-maps';
+// import {getPrediction, formatPrediction} from './Utils';
 
+
+const _ = require('lodash');
 const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
 const Map = () => {
   const [marker, setMarker] = useState('');
+  const [info, setInfo] = useState('');
   const defaultLocation = {lat: 33.6405, lng: -117.8443};
 
   const EmbeddedMap = compose(
@@ -16,33 +20,39 @@ const Map = () => {
         googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
         loadingElement: <div style={{height: `100%`}} />,
         containerElement: <div style={{height: `400px`}} />,
-        mapElement: <div style={{height: `100%`}} />,
-        currentLocation: defaultLocation,
-        isMarkerShown: true
+        mapElement: <div style={{height: `100%`}} />
       }),
       withScriptjs,
-      withGoogleMap,
+      withGoogleMap
   )((props) => (
     <GoogleMap
-      onClick={(mouseEvent) => {
-        props.currentLocation.lat = mouseEvent.latLng.lat();
-        props.currentLocation.lng = mouseEvent.latLng.lng();
-        setMarker(`${mouseEvent.latLng.lat()}, ${mouseEvent.latLng.lng()}`);
-      }}
       defaultZoom={7}
       defaultCenter={defaultLocation}
+      onClick={(mouseEvent) => {
+        setMarker(`${mouseEvent.latLng.lat()},${mouseEvent.latLng.lng()}`);
+        fetch('/flight_horizon', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: JSON.stringify({rxLatLng: marker})
+        }).then((response) => response.json()).then((data) => {
+          alert('Request Success');
+          setInfo(JSON.stringify(data));
+        });
+      }}
     >
-      {props.isMarkerShown && (
-        <Marker
-          position={props.currentLocation}
-        />
+      {(<Marker position={{lat: marker.lat, lng: marker.lng}} />
       )}
     </GoogleMap>
   ));
 
   return <div className="Map">
     <EmbeddedMap />
-    <p className="Marker">pointer location: {marker}</p>
+    <p className="Marker">Cursor location: {marker}</p>
+    <p className="Marker">Upcoming pass: {info}</p>
   </div>;
 };
 
