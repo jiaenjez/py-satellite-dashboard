676545e44e83b5d322b4747012da43007e9dda40
Chillinthestar <example@uci.edu>
2022-02-14 00:45:59 -0600

    Setup for PostgreSQL
    - Connection details are included in README.md

---
+++ src/python/db-model.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from src.python.app import app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://" \
+                                        "omoglffn:ySoZGmy1jAxRdoNRAvKk-LZLOBcqFrGH@castor.db.elephantsql.com/omoglffn"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.Date)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "created_at": str(self.created_at.strftime('%d-%m-%Y'))
+        }
+
+
+db.create_all()
---
+++ src/python/db-utils.py
@@ -0,0 +1,6 @@
+testQuery = """
+    SELECT table_name
+      FROM information_schema.tables
+     WHERE table_schema='public'
+       AND table_type='BASE TABLE';
+"""
