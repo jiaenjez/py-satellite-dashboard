2f66a5871362de1fc8690777601ce19da0f8a8a1
Chillinthestar <chillinthestar@outlook.com>
2022-02-15 01:13:02 -0600

    Move DB operations to dbUtils.py

--- src/python/dbUtils.py
+++ src/python/dbUtils.py
@@ -1,6 +1,37 @@
-from src.python import appConfig, dbQueries
+from src.python import appConfig, dbQueries, dbModel
 
-dbCursor = appConfig.dbConnection.cursor()
-dbCursor.execute(dbQueries.TLE_FIND_ISS)
-for r in dbCursor.fetchall():
-    print(r)
+
+def dbCommit():
+    dbModel.db.session.commit()
+
+
+def dbWrite(entryArray):
+    for entry in entryArray:
+        dbModel.db.session.add(entry)
+    dbModel.db.session.commit()
+
+
+def dbRead(queryName, *args):
+    dbCursor = appConfig.dbConnection.cursor()
+    if args:
+        dbCursor.execute(dbQueries.queries[queryName](args))
+    else:
+        dbCursor.execute(dbQueries.queries[queryName])
+    return dbCursor.fetchall()
+
+
+def dbDropTable(tableName):
+    dbCursor = appConfig.dbConnection.cursor()
+    dbCursor.execute(dbQueries.queries["drop_table_by_name"](tableName))
+
+
+def dbClose():
+    dbModel.db.close_all_sessions()
+
+
+def dbDropAll():
+    dbModel.db.drop_all()
+
+
+def dbCreateAll():
+    dbModel.db.create_all()
